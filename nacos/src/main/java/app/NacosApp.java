3592e56c3962cf45f1ac3b282574a326d8508342
baizhuangzhuang <15217742393@163.com>
2023-07-15 23:49:03 +0800

    [add] nacos 验证通过

--- nacos/src/main/java/app/NacosApp.java
+++ nacos/src/main/java/app/NacosApp.java
@@ -4,10 +4,8 @@ package app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @EnableDiscoveryClient
-@RefreshScope
 @SpringBootApplication
 public class NacosApp {
     public static void main(String[] args) {
--- nacos/src/main/java/app/controller/TestController.java
+++ nacos/src/main/java/app/controller/TestController.java
@@ -1,17 +1,20 @@
 package app.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RefreshScope
 @RestController
 public class TestController {
 
-    @Value("${userName:DefaultString}")
-    private String userName;
+    @Value("${name}")
+    private String name;
 
     @GetMapping
     public String getInfo(){
-        return userName;
+        System.out.println("getInfo");
+        return name;
     }
 }
