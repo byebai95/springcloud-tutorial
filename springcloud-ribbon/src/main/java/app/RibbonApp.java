b9a9f7c03e4e31d0a0836c91c9f5bc210b6eca0d
baizhuangzhuang <15217742393@163.com>
2023-07-27 22:21:28 +0800

    [add] 增加ribbon模块代码

---
+++ springcloud-ribbon/src/main/java/app/RibbonApp.java
@@ -0,0 +1,11 @@
+package app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RibbonApp {
+    public static void main(String[] args) {
+        SpringApplication.run(RibbonApp.class, args);
+    }
+}
